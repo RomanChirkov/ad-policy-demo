455ac304d4e412ead1297e9db1d7d4ea31b74787
roman_chirkov <roman_chirkov@akta.tech>
2022-05-31 15:45:41 +0300

    cc tracker

---
+++ scripts/captions.js
@@ -0,0 +1,30 @@
+let captionActivated = false;
+let captionLang = null;
+
+
+anvp.onReady = function (p) {
+    p.setListener(generalListener);
+};
+
+function generalListener(e) {
+    // console.log("event: [" + e.name + "]", e.args);
+    switch (e.name) {
+        case "VIDEO_STARTED":
+            if(captionActivated){
+                anvp.p0.setCaption(captionLang);
+            }
+            break;
+        case "SET_CAPTION":
+            let lang = e.args[0]
+            if(lang){
+                captionActivated = true ;
+                captionLang = lang;
+            }else{
+                captionActivated = false;
+                captionLang = null;
+            }
+            break;
+        default:
+            break;
+    }
+  }
